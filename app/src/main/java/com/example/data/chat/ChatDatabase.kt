aac77cdd85d54238c5a31aa7283323675b1a5684
andrarker <114063214+andrarker@users.noreply.github.com>
2026-06-18 00:51:59 +0200

    Add app/src/main/java/com/example/data/chat/ChatDatabase.kt

---
+++ app/src/main/java/com/example/data/chat/ChatDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.data.chat
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ChatMessage::class], version = 1, exportSchema = false)
+abstract class ChatDatabase : RoomDatabase() {
+    abstract fun chatDao(): ChatDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ChatDatabase? = null
+
+        fun getDatabase(context: Context): ChatDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ChatDatabase::class.java,
+                    "nicoleporchetta_chat_db"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
